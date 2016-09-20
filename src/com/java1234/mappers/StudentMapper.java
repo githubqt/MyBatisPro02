e4b510bdda5c59ecc22149274530d33458c9cd42
githubqt <310176154@qq.com>
2016-09-20 10:01:16 +0800

    MyBatis动态sql

---
+++ src/com/java1234/mappers/StudentMapper.java
@@ -0,0 +1,28 @@
+package com.java1234.mappers;
+
+import java.util.List;
+import java.util.Map;
+
+import com.java1234.model.Student;
+
+
+public interface StudentMapper {
+
+	public List<Student> searchStudents(Map<String,Object> map);
+	
+	public List<Student> searchStudents2(Map<String,Object> map);
+	
+	public List<Student> searchStudents3(Map<String,Object> map);
+	
+	public List<Student> searchStudents4(Map<String,Object> map);
+	
+	public List<Student> searchStudents5(Map<String,Object> map);
+	
+	public List<Student> searchStudents6(String name,int age);
+	
+	public int updateStudent(Student student);
+	
+	public int insertStudent(Student student);
+	
+	public Student getStudentById(Integer id);
+}
